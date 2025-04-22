d18c693230d60890020523e2ec9e8a1805294ebd
bingle625 <fhqhxm8899@gmail.com>
2025-04-22 20:55:15 +0900

    test: Post 생성 실패 테스트 코드

--- src/test/java/org/sopt/service/PostServiceTest.java
+++ src/test/java/org/sopt/service/PostServiceTest.java
@@ -12,6 +12,7 @@ import org.sopt.repository.PostRepository;
 import org.sopt.util.TimeIntervalUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -32,9 +33,10 @@ public class PostServiceTest {
   TimeIntervalUtil timeIntervalUtil;
 
   @Nested
+  @DisplayName("Post 생성 테스트")
   class PostCreateTest {
 
-    @DisplayName("Post 생성하기 성공")
+    @DisplayName("성공 - Post 생성하기")
     @Test
     void createPost_ShouldSuccess() {
       // given
@@ -56,6 +58,41 @@ public class PostServiceTest {
       verify(postRepository, times(1)).existsByTitle(dummyTitle);
       verify(postRepository, times(1)).save(any(Post.class));
       verify(timeIntervalUtil, times(1)).startTimer();
+    }
+
+    @DisplayName("실패 - Post 중복된 제목")
+    @Test
+    void createPost_duplicateTitle() {
+      // given
+      String dummyTitle = "dummyTitle";
+
+      given(postRepository.existsByTitle(dummyTitle)).willReturn(true);
+      given(timeIntervalUtil.isAvailable()).willReturn(true);
+
+      // when & then
+      assertThatThrownBy(() -> postService.createPost(dummyTitle))
+          .isInstanceOf(RuntimeException.class)
+          .hasMessageContaining("중복된 제목의 게시물입니다.");
+
+      verify(postRepository, times(0)).save(any(Post.class));
+      verify(timeIntervalUtil, times(0)).startTimer();
+    }
+
+    @DisplayName("실패 - Post 3분 내 재시도")
+    @Test
+    void createPost_TimeInterval() {
+      // given
+      String dummyTitle = "dummyTitle";
+
+      given(timeIntervalUtil.isAvailable()).willReturn(false);
+
+      // when & then
+      assertThatThrownBy(() -> postService.createPost(dummyTitle))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessageContaining("아직 새로운 게시물을 작성하실 수 없습니다.");
+
+      verify(postRepository, times(0)).save(any(Post.class));
+      verify(timeIntervalUtil, times(0)).startTimer();
     }
   }
 }
