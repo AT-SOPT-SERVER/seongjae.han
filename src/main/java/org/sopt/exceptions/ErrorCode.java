6a3535805d64d857892c81b42fa3d328b3f5bf5b
bingle625 <fhqhxm8899@gmail.com>
2025-04-24 00:36:30 +0900

    fix: typo

--- src/main/java/org/sopt/exceptions/ErrorCode.java
+++ src/main/java/org/sopt/exceptions/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
   TOO_MANY_POST_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, "c4290", "게시글은 3분 간격으로만 작성할 수 있습니다."),
 
   // 500번대 (Server Error)
-  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S5000","알 수 없는 서버 오류가 발생했습니다.");
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "s5000","알 수 없는 서버 오류가 발생했습니다.");
 
   private final HttpStatus status;
   private final String code;
