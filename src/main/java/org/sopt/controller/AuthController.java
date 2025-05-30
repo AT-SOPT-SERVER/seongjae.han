7dd864f8284bf1bdd69ef5193b3cab80ac2ad865
bingle625 <fhqhxm8899@gmail.com>
2025-05-30 20:11:29 +0900

    feat: 쿠키 관련 테스트

--- src/main/java/org/sopt/controller/AuthController.java
+++ src/main/java/org/sopt/controller/AuthController.java
@@ -1,11 +1,18 @@
 package org.sopt.controller;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+
+import org.sopt.domain.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +34,49 @@ public class AuthController {
     }
 
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패");
+  }
+
+  @GetMapping("/set-cookie")
+  public ResponseEntity<String> setCookie(
+      HttpServletRequest request,
+      HttpServletResponse response
+  ) {
+    final String userName = "userSopt";
+    final String password = "sopt1234";
+
+    final Cookie userNameCookie = new Cookie("userId", userName);
+    final Cookie passwordCookie = new Cookie("password", password);
+
+    userNameCookie.setPath("/");
+    passwordCookie.setPath("/");
+
+    response.addCookie(userNameCookie);
+    response.addCookie(passwordCookie);
+
+    return ResponseEntity.ok("쿠키가 맛있게 구워졌습니다.");
+  }
+
+  @GetMapping("/get-cookie")
+  public ResponseEntity<String> getCookie(
+      @CookieValue("userId") String userId,
+      @CookieValue("password") String password
+  ) {
+    return ResponseEntity.ok("받은 쿠키 유저 아이디: " + userId + " 유저 비밀번호: " + password);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> loginV2(HttpServletRequest request) {
+    String userId = "userSopt";
+    String password = "sopt1234";
+
+    if (userId.equals("userSopt") && password.equals("sopt1234")) {
+      final HttpSession session = request.getSession(true);
+      session.setAttribute("user", User.of("soptUser", "메일"));
+
+      return ResponseEntity.ok("세션 저장 완료");
+    }
+
+    throw new RuntimeException("로그인 실패");
   }
 
   private static boolean authorizeUser(final String credentials) {
