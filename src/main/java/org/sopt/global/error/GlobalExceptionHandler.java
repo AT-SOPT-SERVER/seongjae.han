db4835fa082e2d470665a7d029b862db9113b960
bingle625 <fhqhxm8899@gmail.com>
2025-06-09 11:08:53 +0900

    feat: 로그 관련 기능 추가

--- src/main/java/org/sopt/global/error/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/error/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ import org.sopt.global.error.exception.ApiException;
 import org.sopt.global.error.exception.ErrorCode;
 import org.sopt.global.response.ApiResponse;
 import org.sopt.global.util.log.LoggingUtil;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -28,15 +32,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
   }
 
-  @ExceptionHandler
-  public ResponseEntity<ApiResponse<Void>> handleGlobalException(Exception exception) {
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      final MethodArgumentNotValidException ex,
+      final HttpHeaders headers, final HttpStatusCode status, final WebRequest request) {
+    this.loggingUtil.error(ex);
 
-    this.loggingUtil.error(exception);
+    ApiResponse<Void> apiResponse = ApiResponse.error(
+        ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
+        ErrorCode.INTERNAL_SERVER_ERROR.getMessage()
+    );
 
-    ApiResponse<Void> apiResponse = ApiResponse.error(ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
-        ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
-
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
   }
-
 }
